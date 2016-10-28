9f2143b23d26bd4d9d14e8221b2622210787357b
Goldtean <jeremyoltean@gmail.com>
2016-10-27 21:56:52 -0600

    New seeds for Picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,12 @@
 User.create(email: "jp@jp.jp", username: "jp", password: "jpjpjpjp")
 User.create(email: "jt@jt.jt", username: "jt", password: "jtjtjtjt")
 User.create(email: "jb@jb.jb", username: "jb", password: "jbjbjbjb")
+
+Picture.create(category: "Jordan", url: "/memepics/1.jpg")
+Picture.create(category: "Kanye Taylor", url: "/memepics/2.jpg")
+Picture.create(category: "Puppy Horse", url: "/memepics/3.jpg")
+Picture.create(category: "Usain Bolt", url: "/memepics/4.jpg")
+Picture.create(category: "Girls", url: "/memepics/5.jpg")
+Picture.create(category: "Oltean Obama", url: "/memepics/6.jpg")
+Picture.create(category: "Prince Charles Brazil", url: "/memepics/7.jpg")
+Picture.create(category: "Rapist Search", url: "/memepics/8.jpg")
